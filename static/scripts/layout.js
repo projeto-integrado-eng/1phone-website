a12460c360cfc037bb2344496dff1ee7f1eda932
ckc-dev <ckc-dev@pm.me>
2020-10-26 17:46:07 -0300

    Made header sticky

---
+++ static/scripts/layout.js
@@ -0,0 +1,8 @@
+// Initialize header
+const header = document.getElementsByTagName("header")[0];
+
+// Enable header shadow based on whether or not user has scrolled farther than header height.
+window.onscroll = () => {
+  header.style.boxShadow =
+    window.scrollY < header.offsetHeight ? "none" : "var(--shadow)";
+};
